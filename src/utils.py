dde113bbdb113160bf6bf8a09ef65161fb8070ec
ankitpatil3003 <apatil@binghamton.edu>
2024-09-26 19:34:23 -0400

    Hyperparameter Tuning done

--- src/utils.py
+++ src/utils.py
@@ -1,43 +1,47 @@
 import os
 import sys
-
-import numpy as np
-import pandas as pd
 import dill
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
-
     except Exception as e:
         raise CustomException(e, sys)
-    
-def evaluate_models(X_train, y_train, X_test, y_test,models):
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
+        # Iterate over each model and its parameters
+        for i, (model_name, model) in enumerate(models.items()):
+            # Get the parameters for the specific model
+            params = param.get(model_name, {})
 
-            model.fit(X_train, y_train)  # Train model
+            # Perform GridSearchCV using the model and its parameters
+            gs = GridSearchCV(model, params, cv=3)
+            gs.fit(X_train, y_train)
 
+            # Set the best parameters from GridSearchCV
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            # Make predictions
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
 
+            # Compute R2 score for training and test sets
             train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            # Save the test score in the report
+            report[model_name] = test_model_score
 
         return report
 
